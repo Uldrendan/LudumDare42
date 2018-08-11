4b4cc042b5aecbdd0301b2ed19ab3e002a6139e2
Daniel Clarke <d.clarke@bearcloudgames.com>
2018-08-11 15:09:30 -0400

    Some demo sprites and a player input script

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+    public GameObject[] passengers;
+
+	void Update () {
+        if(Input.GetMouseButtonDown(0)){
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Instantiate(passengers[Random.Range(0, passengers.Length)], new Vector3(mousePos.x,mousePos.y,0), Quaternion.identity);
+        }
+	}
+}
