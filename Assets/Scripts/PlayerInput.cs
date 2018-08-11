efd0244e7c9f1edd704c198235419c66209e509d
Daniel Clarke <d.clarke@bearcloudgames.com>
2018-08-11 18:27:14 -0400

    Logic to help control trajectory of shots

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -5,11 +5,32 @@ using UnityEngine;
 public class PlayerInput : MonoBehaviour {
 
     public GameObject[] passengers;
+    public float maximumForce = 10;
+    public float minimumForce = 1;
+
+    GameObject currentPassenger;
+    bool aiming;
+    Vector2 aimVector;
+    Vector3 startPoint;
 
 	void Update () {
+        if (aiming)
+        {
+            Vector2 heading = Camera.main.ScreenToWorldPoint(Input.mousePosition) - startPoint;
+            float distance = heading.magnitude;
+            aimVector = (heading / distance) * -1;
+        }
+
+        if(Input.GetMouseButtonUp(0)){
+            currentPassenger.GetComponent<Rigidbody2D>().gravityScale = 1;
+            currentPassenger.GetComponent<Rigidbody2D>().AddForce(aimVector* 10,ForceMode2D.Impulse);
+            aiming = false;
+        }
         if(Input.GetMouseButtonDown(0)){
-            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Instantiate(passengers[Random.Range(0, passengers.Length)], new Vector3(mousePos.x,mousePos.y,0), Quaternion.identity);
+            currentPassenger = Instantiate(passengers[Random.Range(0, passengers.Length)], transform.position, Quaternion.identity);
+            currentPassenger.GetComponent<Rigidbody2D>().gravityScale = 0;
+            startPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            aiming = true;
         }
 	}
 }
